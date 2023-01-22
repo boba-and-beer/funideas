fb3abef0953737e116dfd3f6b4832749a66b052c
Jacky Wong <jacky.wong@relevance.ai>
2023-01-22 21:30:41 +1100

    deploy random

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
 
-st.title('Uber pickups in NYC')
+st.title('J & A Random Idea Generator')
+
+DATES = """Play your favorite board games
+Play strip Twister (or Monopoly) or any game you like
+Build a blanket fort
+At home stargazing
+Paint together
+Put together a craft
+Have a DIY spa and pamper yourselves
+Write down your bucket list
+Ask each other deep questions
+Learn a new skill from YouTube"""
+
+DATES = DATES.split('\n')
+
+import random
+
+if st.button("press here for random date idea"):
+	RANDOM_DATE = random.choice(DATES)
+	st.title(RANDOM_DATE)
 
